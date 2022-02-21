e46d1d0e04b483344a5f2a02e5c1a3143ca064d5
ashmeetkour18 <ashmeetkour18@gmail.com>
2022-02-21 18:59:05 +0530

    Assessment 1

---
+++ src/Assessment1/Question2LessComplexity.java
@@ -0,0 +1,20 @@
+package Assessment1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Question2LessComplexity {
+    public static void main(String[] args) {
+        List<Integer> integerList= Arrays.asList(1,2,3,4,5,6,7);
+        int sum=17;
+        System.out.println(addSum(integerList,sum));
+    }
+
+    private static boolean addSum(List<Integer> integerList, int sum) {
+        for(int i=0;i<integerList.size();i++){
+            if(integerList.contains(sum-integerList.get(i)))
+                return true;
+        }
+        return false;
+    }
+}
