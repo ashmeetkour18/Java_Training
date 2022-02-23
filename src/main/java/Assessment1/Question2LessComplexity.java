8ed8dde6cc8753e28a5dcf30e3dbf353b9b8a13c
ashmeetkour18 <ashmeetkour18@gmail.com>
2022-02-23 10:30:11 +0530

    Assessment 1

--- src/main/java/Assessment1/Question2LessComplexity.java
+++ src/main/java/Assessment1/Question2LessComplexity.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 public class Question2LessComplexity {
     public static void main(String[] args) {
-        List<Integer> integerList= Arrays.asList(1,2,3,4,5,6,7);
-        int sum=17;
+        List<Integer> integerList= Arrays.asList(1,2,3,4,5,6,7,7);
+        int sum=14;
         System.out.println(addSum(integerList,sum));
     }
 
     private static boolean addSum(List<Integer> integerList, int sum) {
         for(int i=0;i<integerList.size();i++){
-            if(integerList.contains(sum-integerList.get(i)))
+            int number= sum-integerList.get(i);
+            if(integerList.contains(number)&& integerList.indexOf(number)!=i)
                 return true;
         }
         return false;
